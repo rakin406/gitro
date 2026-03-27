1771ca2b014635bd0f889b847b86278f2a90f70f
Rakin Rahman <rakinhacker@gmail.com>
2026-03-28 01:56:12 +0600

    feat: add textarea

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,14 @@ export default function Home() {
   return (
     <main className="flex flex-col flex-1 items-center justify-center font-sans">
       <div className="flex flex-col w-sm md:w-xl rounded-xl bg-foreground text-background overflow-hidden shadow-lg gap-4 p-4">
-        <p className="cursor-default">
-          Insert GitHub repositories (separated by spaces)
-        </p>
+        <label htmlFor="repos" className="block cursor-default">Insert GitHub repositories (separated by spaces)</label>
+
+        <textarea
+          id="repos"
+          rows={4}
+          className="bg-neutral-secondary-medium border border-default-medium rounded-lg block w-full p-3.5 shadow-xs placeholder:text-body resize-none"
+          placeholder="github.com/rakin406/gitro">
+        </textarea>
 
         <div className="flex justify-center">
           <a
