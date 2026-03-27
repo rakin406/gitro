414b3154c0b29656da74fe6916eb99926762d121
Rakin Rahman <rakinhacker@gmail.com>
2026-03-28 01:34:01 +0600

    feat: make responsive button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 export default function Home() {
   return (
     <main className="flex flex-col flex-1 items-center justify-center font-sans">
-      <a
-        className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Search
-      </a>
+      <div className="flex flex-col w-sm md:w-xl rounded-xl bg-foreground text-background overflow-hidden shadow-lg gap-4 p-4">
+        <p className="cursor-default">
+          Insert GitHub repositories (separated by spaces)
+        </p>
+
+        <div className="flex justify-center">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-[#E93636] px-5 text-zinc-50 md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Search
+          </a>
+        </div>
+      </div>
     </main>
   );
 }
