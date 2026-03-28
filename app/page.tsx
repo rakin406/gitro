8a67ca94e55c82c111a5659181efec32bee6fa9d
Rakin Rahman <rakinhacker@gmail.com>
2026-03-28 20:28:29 +0600

    feat: use button instead of link

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,11 @@ export default function Home() {
         </textarea>
 
         <div className="flex justify-center">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-[#E93636] px-5 text-zinc-50 md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <button
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-[#E93636] px-5 text-zinc-50 md:w-[158px] cursor-pointer"
           >
             Search
-          </a>
+          </button>
         </div>
       </div>
     </main>
