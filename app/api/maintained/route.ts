45c98f7151ecc2c92c978f0441b416666af74c27
Rakin Rahman <rakinhacker@gmail.com>
2026-03-28 13:03:13 +0600

    feat: add regex

--- app/api/maintained/route.ts
+++ app/api/maintained/route.ts
@@ -1,5 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
+import * as z from "zod";
+
+const schema = z.object({
+  // Repositories
+  repos: z
+    .string()
+    .regex(
+      /^((http|https):\/\/)?(www\.)?github\.com\/([a-zA-Z0-9](?:-(?=[a-zA-Z0-9])|[a-zA-Z0-9]){0,38}(?<=[a-zA-Z0-9]))\/\S+$/,
+    ),
+});
 
 export async function GET(req: NextRequest) {
+  const parsed = schema.parse(req.body);
   return NextResponse.json({});
 }
