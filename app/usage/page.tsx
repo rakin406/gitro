7cb9e504134404a1125e6dfddef3903501cf5b8c
Rakin Rahman <rakinhacker@gmail.com>
2026-03-29 21:53:47 +0600

    feat: set width

--- app/usage/page.tsx
+++ app/usage/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      <div className="relative bottom-20 text-center leading-8">
+      <div className="relative bottom-20 w-sm md:w-xl text-center leading-8">
         <h2>
           <em><b>Gitro</b></em> lets you check the most maintained GitHub repositories.
         </h2>
