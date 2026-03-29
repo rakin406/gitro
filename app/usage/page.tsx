34411d6becd9476be625aa2459557b3042ec1dad
Rakin Rahman <rakinhacker@gmail.com>
2026-03-29 21:27:47 +0600

    feat: add usage page

---
+++ app/usage/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <div className="relative bottom-20 text-center leading-8">
+        <h2>
+          <em><b>Gitro</b></em> lets you check the most maintained GitHub repositories.
+        </h2>
+
+        <p><b>Usage:</b></p>
+      </div>
+    </div>
+  );
+}
